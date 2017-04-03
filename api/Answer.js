8eac7a02b4da0b5f00b57a0f0995865feae31c14
sunchenguang <809200299@qq.com>
2017-04-03 21:05:57 +0800

    update: 移除注释

--- api/Answer.js
+++ api/Answer.js
@@ -20,68 +20,6 @@ let _renderUrl = (answerId) => {
  *                  "/question/28207685/answer/39974928",
  *                  the answerId of this post is "11382008" instead.
  */
-// let voters1 = (answerId) => {
-//   let url = _renderUrl(answerId)
-//   let options = {
-//     url
-//   }
-//
-//   return request(options).then(function (res) {
-//     let buffer = JSON.parse(res.body)
-//     let voters = []
-//
-//     if (Array.isArray(buffer.payload)) {
-//       voters = buffer.payload.map(function (payload) {
-//         let $, user, status, anchor
-//
-//         $ = cheerio.load(payload)
-//         user = {}
-//         anchor = $('a[title]')
-//         status = $('ul.status > li').children('a, span')
-//
-//         user.name = anchor.attr('title')
-//         user.anonymous = !user.name
-//
-//         if (!user.anonymous) {
-//           user.profileUrl = anchor.attr('href')
-//           user.sex = (function (str) {
-//             switch (str) {
-//               case '他':
-//                 return 'male'
-//               case '她':
-//                 return 'female'
-//               default:
-//                 return undefined
-//             }
-//           })($('.zg-btn-follow').text().slice(2))
-//         } else {
-//           user.name = '匿名用户'
-//         }
-//
-//         user.avatar = $('.zm-item-img-avatar').attr('src')
-//         user.like = parseInt(status.eq(0).text())
-//         user.thank = parseInt(status.eq(1).text())
-//         user.question = (function (el) {
-//           let href = el.attr('href')
-//           if (href) {
-//             this.questionUrl = href
-//           }
-//           return parseInt(el.text())
-//         }.call(user, status.eq(2)))
-//         user.answer = (function (el) {
-//           let href = el.attr('href')
-//           if (href) {
-//             this.answerUrl = href
-//           }
-//           return parseInt(el.text())
-//         }.call(user, status.eq(3)))
-//
-//         return user
-//       })
-//     }
-//     return voters
-//   })
-// }
 
 let voters = async (answerId) => {
   try {
